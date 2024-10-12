b46f6aa13d34173fbecb07fe51979bbe81ea1cfc
Haritha Thiagu Kumar <haritha.vf130@gmail.com>
2024-10-11 21:56:12 -0500

    testng runner maven cross parallel testing incognitoheadless highlight threadlocal

--- src/main/java/com/qa/waf/factory/DriverFactory.java
+++ src/main/java/com/qa/waf/factory/DriverFactory.java
@@ -10,57 +10,115 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import com.qa.waf.frameworkexceptions.FrameException;
+
 public class DriverFactory {
 	WebDriver driver;
-	Properties prop=new Properties();
-	
+	OptionsManager options;
+	public static String highlightElement;
+	public static ThreadLocal<WebDriver> tlDriver=new ThreadLocal<WebDriver>();
+	//Properties prop = new Properties();
+
 	public DriverFactory(WebDriver driver) {
-		this.driver=driver;
+		this.driver = driver;
 	}
-	
 
 	public WebDriver init_driver(Properties prop) {
-		String browser=prop.getProperty("browser");
+		String browser = prop.getProperty("browser");
+		options=new OptionsManager(prop);
+		highlightElement=prop.getProperty("highlight");
 		switch (browser.toLowerCase().trim()) {
 		case "chrome":
-			driver = new ChromeDriver();
+			//driver = new ChromeDriver(options.getChromeOptions());
+			tlDriver.set(new ChromeDriver(options.getChromeOptions()));
+			System.out.println("Opening Chrome browser");
 			break;
 		case "edge":
-			driver = new EdgeDriver();
+			//driver = new EdgeDriver(options.getEdgeOptions());
+			tlDriver.set(new EdgeDriver(options.getEdgeOptions()));
+			System.out.println("Opening Edge browser");
 			break;
 		case "firefox":
-			driver = new FirefoxDriver();
+			//driver = new FirefoxDriver(options.getFirefoxOptions());
+			tlDriver.set(new FirefoxDriver(options.getFirefoxOptions()));
+			System.out.println("Opening Firefox browser");
 			break;
 		default:
-			System.out.println("Please enter a valid browser name.You have entered:"+browser);
+			System.out.println("Please enter a valid browser name.You have entered:" + browser);
 			break;
 		}
-		driver.manage().window().maximize();
-		driver.manage().deleteAllCookies();
-		driver.get(prop.getProperty("url"));
-		return driver;
+		getDriver().manage().window().maximize();
+		getDriver().manage().deleteAllCookies();
+		getDriver().get(prop.getProperty("url"));
+		return getDriver();
 
+	}
+	
+	public synchronized WebDriver getDriver() {
+		
+		return tlDriver.get();
 	}
 
 	public Properties init_prop() {
-		
+		Properties prop = new Properties();
+		FileInputStream ip = null;
+		// mvn clean install -Denv="qa"
+		String envName = System.getProperty("env");
+		System.out.println("Running testcases on:"+envName);
+
 		try {
-			FileInputStream ip=new FileInputStream("./src/main/resource/config/config.properties");
-			try {
-				prop.load(ip);
-			} catch (IOException e) {
-				
-				e.printStackTrace();
+
+			if (envName == null) {
+				System.out.println("No environment specified.Hence running in default QA environment");
+				ip = new FileInputStream("./src/main/resource/config/config.properties");
+
 			}
-		} 
-		
-		catch (FileNotFoundException e) {
+			else {
+				
+				switch (envName.toLowerCase().trim()) {
+				case "dev":	
+					ip = new FileInputStream("./src/main/resource/config/devConfig.properties");
+					break;
+				case "stage":	
+					ip = new FileInputStream("./src/main/resource/config/stageConfig.properties");
+					break;
+				case "prod":
+					ip = new FileInputStream("./src/main/resource/config/prodConfig.properties");
+					break;
+				case "qa":
+					
+					ip = new FileInputStream("./src/main/resource/config/config.properties");
+					break;	
+
+				default:
+					System.out.println("Plz pass the right environment.The given environment is"+envName);
+					throw new FrameException("NOVALIDENVGIVEN");
+					
+				}
 			
+			}
+
+		}
+
+		catch (FileNotFoundException e) {
+
 			e.printStackTrace();
 		}
-		
+
+		try {
+			prop.load(ip);
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+		System.out.println("Running testcases in headless=:"+prop.getProperty("headless"));
+		System.out.println("Running testcases in incognito=:"+prop.getProperty("incognito"));
+
 		return prop;
+		
 
 	}
+	
+	
 
 }
--- src/test/java/com/qa/waf/basetest/BaseTest.java
+++ src/test/java/com/qa/waf/basetest/BaseTest.java
@@ -5,6 +5,7 @@ import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.asserts.SoftAssert;
 
 import com.qa.waf.factory.DriverFactory;
@@ -27,11 +28,16 @@ public class BaseTest {
 	protected Properties prop;
 	protected SoftAssert sa;
 
+	@Parameters({"browser"})
 	@BeforeTest
-	public void setUp() {
+	public void setUp(String browservalue) {
 
 		df = new DriverFactory(driver);
 		prop = df.init_prop();
+		
+		if(browservalue!=null) {
+			prop.setProperty("browser", browservalue);
+		}
 		driver = df.init_driver(prop);
 		sa=new SoftAssert();
 		lp = new LoginPage(driver);
